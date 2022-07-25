d0f39fa69b025dda1dd3fdabf38d926741f6bf2a
tolgatitizz <tolgatitizz@hotmail.com>
2022-07-25 14:31:33 +0300

    Time Slot Controller Eklendi Slot Seçim Tablosu Eklendi

---
+++ Trial/Controllers/TimeSlotController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Trial.Controllers
+{
+    public class TimeSlotController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Select()
+        {
+            return View();
+        }
+    }
+}
