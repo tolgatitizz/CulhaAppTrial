afe71d5695c5d2e6ae11163db251cd46a57a8c3b
serenakdemir <serenakdemir@hotmail.com>
2022-08-05 16:15:01 +0300

    Class Schedule Gösterim tablosu oluşturuldu

---
+++ Trial/Models/ClassSchedule.cs
@@ -0,0 +1,12 @@
+﻿namespace Trial.Models
+{
+    public class ClassSchedule
+    {
+        public int? Id { get; set; }
+        public int? ClassScheduleItemNo { get; set; }
+        public string? ClassroomCode { get; set; }
+        public int? TimeSlotId { get; set; }
+        public int? CourseOfferedId { get; set; }
+    }
+}
+
---
+++ Trial/ViewModels/ClassScheduleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Trial.APIViewModels;
+using Trial.Models;
+namespace Trial.ViewModels
+{
+    public class ClassScheduleViewModel
+    {
+        public List<ClassSchedule>? ClassSchedules { get; set; }
+        public ClassSchedule? ClassSchedule { get; set; }
+        public ClassScheduleRequestModel? ClassScheduleRequestModel { get; set; }
+    }
+}
