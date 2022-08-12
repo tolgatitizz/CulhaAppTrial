ab76e335d973310c170925f2c3bb280125a3dfda
serenakdemir <serenakdemir@hotmail.com>
2022-08-12 13:00:57 +0300

    DayClassSchedule tablosu oluşturuldu

---
+++ Trial/APIViewModels/DayClassScheduleRequestModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Trial.APIViewModels
+{
+    public class DayClassScheduleRequestModel
+    {
+       
+        public string? ClassroomCode { get; set; }
+       
+        public string? CourseName { get; set; }
+        public int? SemesterCount { get; set; }
+        public string? DepartmentName { get; set; }
+        public string? AcademicianFullName { get; set; }
+        public int? Slot { get; set; }
+    }
+}
